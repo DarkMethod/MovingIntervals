ea5f0150599e18545c55c035877f07ca298a463a
Rohit Markam <mrmarkam@gmail.com>
2018-02-28 18:35:11 +0530

    Solution V1

--- index.py
+++ index.py
@@ -12,14 +12,14 @@ def overlaps_exists(child):
             overlaps = 1
     return overlaps, num_of_overlaps
 
-T = int(raw_input("No. of testcases: "))
+T = int(raw_input())
 output = []
 for i in range(0,T):
-    C, N, K = raw_input("No. of Cakes, Children and K in "+str(i+1)+" testcase: ").split()
+    C, N, K = raw_input().split()
     C, N, K = int(C), int(N), int(K)
     child = []
     for j in range(0,N):
-        c = [int(x) for x in raw_input("Start pos, End pos for "+str(j+1)+" child: ").split()]
+        c = [int(x) for x in raw_input().split()]
         child.append(c)
     if K == 0:
         if N > C:
@@ -30,20 +30,36 @@ for i in range(0,T):
             if overlaps == 0:
                 output.append("GOOD")
             elif overlaps == 1:
-            elif K == 1:
                 output.append("BAD")
+    elif K == 1:
         if N > C:
             output.append("BAD")
         else:
-            overlaps, num_of_overlaps = 0,0
-            overlaps, num_of_overlaps = overlaps_exists(child)
-            if overlaps == 0:
-                output.append("GOOD")
-            elif overlaps == 1:
-                if num_of_overlaps > 1:
-                    output.append("BAD")
-                else:
-                        
+            demand = 0
+            for i in child:
+                demand += i[1]-i[0]+1
+            if demand > C:
+                output.append("BAD")
+            else:
+                overlaps, num_of_overlaps = 0,0
+                overlaps, num_of_overlaps = overlaps_exists(child)
+                if overlaps == 0:
+                    output.append("GOOD")
+                elif overlaps == 1:
+                    num_overlaps = [0]*len(child)
+                    max = 0
+                    for i in range(len(child)):
+                        for j in range(i+1, len(child)):
+                            if child[i][1] >= child[j][0]:
+                                num_overlaps[i] += 1
+                                num_overlaps[j] += 1
+                    for i in range(len(child)):
+                        if num_overlaps[i] > num_overlaps[max]:
+                            max = i
+                    if (child[max][1]+child[max][0]-1) > (C-demand+child[max][0]+child[max][1]-1):
+                        output.append("BAD")
+                    else:
+                        output.append("GOOD")
 
 for o in range(len(output)):
     print output[o]
